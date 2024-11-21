cc9f37ddfaac3a458055c116ea59540317c3c2a6
raditanurhasna <raditanurhasna1410@gmail.com>
2024-11-21 16:43:37 +0700

    enum class dengan nama Halaman yang memiliki dua nilai atau konstanta, yaitu Form dan Data.

---
+++ app/src/main/java/com/example/simpleviewmodel/Navigasi.kt
@@ -0,0 +1,27 @@
+package com.example.simpleviewmodel
+
+import android.view.View
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHost
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.project.model.ListGender
+import com.example.project.model.ListGender.listJk
+import com.example.project.ui.view.FormMahasiswaView
+import com.example.project.ui.viewmodel.MahasiswaViewModel
+import com.example.simpleviewmodel.ui.view.DetailMahasiswaView
+
+enum class Halaman{
+    Form,
+    Data
+}
+
